c8547bbcbc84bda567eea34c42fd828e823ba050
Andrew Loomis <andrew@thanics.com>
2019-01-21 22:53:34 -0500

    added more interface to player class

--- player.h
+++ player.h
@@ -9,6 +9,12 @@ class Player
 public:
     Player();
     Color pieceColor() const { return color; }
+    QString getUsername() const { return username == "" ?
+                    "Unknown" : username; }
+    void setUsername(QString name) { username = name; }
+    bool isLoggedIn() { return username == "" ? false : true; }
+    void logout() { username = ""; }
+
 private:
     QString username;
     Color color = Color::Grey;
